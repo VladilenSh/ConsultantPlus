24a3c0846fd75fcf737d757dc23d865b5791c874
VladilenSh <springs@yandex.ru>
2022-07-09 20:58:42 +0700

    entity

---
+++ Data/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsultantPlus.Data
+{
+    internal class AppContext: DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
