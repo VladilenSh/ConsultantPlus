9f74e17beb4e2c0735a173288e8b441cb9bb84a0
Vlad <springs@yandex.ru>
2022-07-13 21:06:11 +0700

    +

--- obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
@@ -157,7 +157,7 @@ namespace ConsultantPlus {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ConsultantPlus;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ConsultantPlus;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
